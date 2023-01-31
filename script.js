5017b74248c1daa1b0eadbabcfdc10d99a9ea272
Richard Pilipenko <richardpilipenko@Jennys-MacBook-Pro.local>
2023-01-31 19:22:26 +0000

    Finished project

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const formulaOneTeams = {
     teamPrincipals: ['Christian Horner', 'Toto Wolff', 'Mike Krack', 'Franz Tost', 'Frederic Vasseur', 'Andrea Stella'],
-    drivers: ['Lewis Hamilton', 'Max Vestappen', 'Fernando Alonso', 'Valteri Bottas', 'Sergio Perez', 'Ayrton Senna', 'Michael Schumaher', 'Jenson Button', 'George Russel', 'Charles Leclerc', 'Carlos Sainz'],
+    drivers: ['Lewis Hamilton', 'Lando Norris', 'Max Vestappen', 'Kevin Magnussen', 'Fernando Alonso', 'Esteban Ocon', 'Valtteri Bottas', 'Sergio Perez', 'Oscar Piastri', 'Pierre Gasly', 'Zhou Guanyu', 'George Russel', 'Charles Leclerc', 'Carlos Sainz', 'Nico Hulkenberg', 'Lance Stroll', 'Nyck de Vries', 'Logan Sargeant', 'Yuki Tsunoda'],
     fOneTeams: ['Ferrari', 'Mercedes', 'Aston Martin', 'Alpine', 'Williams', 'Alfa Romeo', 'Red Bull', 'AlphaTauri', 'McLaren', 'Haas']
 };
 
 // For picking a random item in an array
 const randomItem = (array) => array[Math.floor(Math.random() * array.length)];
 
+const checkIfPicked = (driverOne, driverTwo) => driverOne[0] !== driverTwo ? true : false;
+
+const findTeam = (array) => {
+    let team = randomItem(array);
+
+    return team.toUpperCase();
+};
+
+const findTeamPrincipal = (array) => randomItem(array);
+
 const findDrivers = (array) => {
     let fOneDrivers = [];
     let driver = '';
-
     
     while (fOneDrivers.length < 2) {
         driver = randomItem(array);
-        checkIfAssigned(fOneDrivers, driver) == true ? fOneDrivers.push(driver) : console.log(driver);
+        checkIfPicked(fOneDrivers, driver) == true ? fOneDrivers.push(driver) : console.log(driver);
     }
-    //console.log(`Your drivers are ${fOneDrivers[0]} and ${fOneDrivers[1]}`);
     return fOneDrivers;
 };
 
-const checkIfAssigned = (driverOne, driverTwo) => driverOne[0] !== driverTwo ? true : false;
+const assembleTheTeam = () => {
+    let drivers = findDrivers(formulaOneTeams.drivers);
+    let completeTeamIs = `Congradulations, you are now the new owner of ${findTeam(formulaOneTeams.fOneTeams)} Formula 1 team!\nThe team principal is ${findTeamPrincipal(formulaOneTeams.teamPrincipals)}.\nDriver one is: ${drivers[0]}\nDriver two is: ${drivers[1]}`;
 
-console.log(findDrivers(formulaOneTeams.drivers));
+    return completeTeamIs;
+}
+
+console.log(assembleTheTeam());
